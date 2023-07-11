d68ebed5818664cf475df84d2657cda9862b8093
iGoodw1n <121541489+iGoodw1n@users.noreply.github.com>
2023-07-11 15:44:11 +0400

    End of section 14

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -86,9 +86,19 @@ export class MembersService {
     return this.http.delete(this.baseUrl + 'users/delete-photo/' + photoId);
   }
 
+  addLike(username: string) {
+    return this.http.post(this.baseUrl + 'likes/' + username, {})
+  }
+
+  getLikes(predicate: string, pageNumber, pageSize) {
+    let params = this.getPaginationHeaders(pageNumber, pageSize);
+    params = params.append('predicate', predicate); 
+    return this.getPaginatedResult<Partial<Member[]>>(this.baseUrl + 'likes', params);
+  }
+
   private getPaginatedResult<T>(url, params) {
     const paginatedResult: PaginatedResult<T> = new PaginatedResult<T>();
-    return this.http.get<T>(url, { observe: 'response', params }).pipe(
+    return this.http.get<T>(url, { observe: 'response', params: params }).pipe(
       map(response => {
         paginatedResult.result = response.body;
         if (response.headers.get('Pagination') !== null) {
